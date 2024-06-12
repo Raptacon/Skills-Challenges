d0a7bb25269c2c0e813f8ddf10ba3aba380eb47c
Jordan Durham <jtd_29@yahoo.com>
2024-06-11 21:22:05 -0600

    Foundations: some bug fixes of the complete code

--- foundations_robot_prog/complete_code/robot.py
+++ foundations_robot_prog/complete_code/robot.py
@@ -12,17 +12,17 @@ class WestCoastRobot(commands2.TimedCommandRobot):
     def robotInit(self):
         super().__init__()
 
-        left_front_motor = phoenix5.WPI_TalonFX(30)
-        right_front_motor = phoenix5.WPI_TalonFX(20)
-        left_back_motor = phoenix5.WPI_TalonFX(31)
-        right_back_motor = phoenix5.WPI_TalonFX(21)
+        right_front_motor = phoenix5.WPI_TalonFX(31)
+        right_back_motor = phoenix5.WPI_TalonFX(30)
+        left_back_motor = phoenix5.WPI_TalonFX(20)
+        left_front_motor = phoenix5.WPI_TalonFX(21)
 
         left_drive_motors = wpilib.MotorControllerGroup(left_front_motor, left_back_motor)
         right_drive_motors = wpilib.MotorControllerGroup(right_front_motor, right_back_motor)
 
         self.drivetrain = WestCoastDrivetrain(left_drive_motors, right_drive_motors)
 
-        self.driver_controller = wpilib.XboxController(0)
+        self.driver_controller = commands2.button.CommandXboxController(0)
 
 
     def disabledInit(self):
@@ -38,16 +38,16 @@ class WestCoastRobot(commands2.TimedCommandRobot):
         pass
 
     def teleopInit(self):
-        if False:
+        if True:
             self.drivetrain.setDefaultCommand(
                 TankDrive(
                     lambda: wpimath.applyDeadband(
-                        self.driver_controller.getRawAxis(self.driver_controller.Axis.kLeftY),
+                        self.driver_controller.getLeftY(),
                         0.1,
                         1
                     ),
                     lambda: wpimath.applyDeadband(
-                        self.driver_controller.getRawAxis(self.driver_controller.Axis.kLeftY),
+                        self.driver_controller.getRightY(),
                         0.1,
                         1
                     ),
@@ -58,12 +58,12 @@ class WestCoastRobot(commands2.TimedCommandRobot):
             self.drivetrain.setDefaultCommand(
                 ArcadeDrive(
                     lambda: wpimath.applyDeadband(
-                        self.driver_controller.getRawAxis(self.driver_controller.Axis.kLeftY),
+                        self.driver_controller.getLeftY(),
                         0.1,
                         1
                     ),
                     lambda: wpimath.applyDeadband(
-                        self.driver_controller.getRawAxis(self.driver_controller.Axis.kRightX),
+                        self.driver_controller.getRightX(),
                         0.1,
                         1
                     ),
@@ -73,6 +73,7 @@ class WestCoastRobot(commands2.TimedCommandRobot):
 
     def teleopPeriodic(self):
         pass
+        #print(f"Current status of A button: {self.driver_controller.a()}")
 
     def testInit(self):
         pass
