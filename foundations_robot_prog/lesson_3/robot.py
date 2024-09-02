8ec1d29e0b9513ad7600ed6818d57e2b9844708d
Jordan Durham <jtd_29@yahoo.com>
2024-09-02 17:06:53 -0600

    Foundations: clean up lesson 3 development instructions

--- foundations_robot_prog/lesson_3/robot.py
+++ foundations_robot_prog/lesson_3/robot.py
@@ -4,28 +4,6 @@ import wpilib
 
 from subsystems.drivetrain import WestCoastDrivetrain
 
-####################################################################################
-# Welcome to Lesson 3! Now that we've moved one side of the drivetrain by using
-# the motors, let's expand upon this and actuate the other side of the drivetrain.
-# By having control of both sides, we'll be able to drive the robot!
-
-# We can now start to think about the whole drivetrain as a subsystem. Subsystems
-# are an integrated set of components that work together to provide a focused
-# range of functionality. For example, an arm subsystem may consist of three motors:
-# one to raise the arm, one to extend it, and another to run a piece grabber.
-# Drivetrain subsystems are typically comprised of the drive motors and sensors
-# that track the distance driven and the heading (direction) of the robot. 
-
-# If you look at the package imports above, you'll notice that we import our own
-# drivetrain class from our own subsystems folder. It is generally advised to
-# organize Python code that either provides similar functionality or operates
-# temporally in a common part of the program's overall control flow in
-# the same folders/modules. Part of this lesson will require you to write code
-# within the WestCoastDrivetrain class.
-
-# Look for "FOR YOU" within the code - this signals that you have something to do 
-####################################################################################
-
 
 class WestCoastRobot(commands2.TimedCommandRobot):
     """
@@ -53,17 +31,7 @@ class WestCoastRobot(commands2.TimedCommandRobot):
         self.driver_controller = wpilib.XboxController(0)
 
         # ***
-        # FOR YOU: we're going to instantiate the drivetrain using our custom class.
-        # If you have not finished coding up WestCoastDrivetrain, please open
-        # the /subsystems/drivetrain.py file, follow the instructions, and come back.
-
-        # Here, we'll create a drivetrain instance attribute within our robot - 
-        # we want to be able to use the drivetrain object in the robot methods below
-        # while ensuring that data associated with the drivetrain is maintained.
-        # To do this, set an instance attribute to be a constructed WestCoastDrivetrain
-        # object, passing the two motor groups for the two sides of the robot as
-        # arguments to the constructor. This will be analogous to the following:
-        # self.subsystem = Subsystem(self.some_motors, self.other_motors)
+        # FOR YOU (3.5): 
 
         # <your code here>
 
@@ -110,33 +78,7 @@ class WestCoastRobot(commands2.TimedCommandRobot):
         with the drivetrain and/or to actuate our mechanisms.
         """
         # ***
-        # FOR YOU: last lesson we used one joystick to operate one side of the robot -
-        # now, we'll use the other joystick to operate the other side of the robot too.
-        # As part of the drivetrain class, you created a tankDrive method (see 
-        # /subsystem/drivetrain.py for more details or to write that code if you haven't
-        # already). We're going to pass the up/down values of each of the joysticks
-        # as arguments to the tankDrive method. Remember - pressing all the way up
-        # on a joystick gives a Y value of -1, while pressing all the way down gives
-        # a Y value of 1.
-
-        # You'll need three methods to operate the robot using tank drive:
-
-        # <your_drivetrain_attribute>.tankDrive(): passes motor voltage percentage
-        # values (1 = 100%, -1 = 100% in opposite direction) to each side of the
-        # robot directly in order to drive. The code here will be analagous to the
-        # following:
-        # self.subsystem.operate(leftInput, rightInput)
-
-        # <your_controller_attribute>.getLeftY(): retrieves how far the left joystick
-        # has been pressed up/down as a numeric value between -1 and 1. The code
-        # will be analagous to the following:
-        # self.device.getJoystickVerticalPercentage()
-
-        # <your_controller_attribute>.getRightY(): same as getLeftY() but for the right
-        # joystick
-
-        # Call the drivetrain's tankDrive method, passing it the left and right
-        # joystick Y values
+        # FOR YOU (3.6)
 
         # <your code here>
 
