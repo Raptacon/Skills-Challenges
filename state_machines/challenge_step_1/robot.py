f8e4c9a3d89ae8cde237fd364c23854b0edc4863
hrf0709 <hfairchild53@gmail.com>
2026-02-07 11:32:11 -0700

    now have a 2D field, but robot is not able to move yet in teleop.

--- state_machines/challenge_step_1/robot.py
+++ state_machines/challenge_step_1/robot.py
@@ -1,12 +1,15 @@
 import wpilib
+from wpilib import SmartDashboard, Field2d
 import ntcore
 import photonlibpy
 from robotpy_apriltag import AprilTagFieldLayout, AprilTagField
 from typing import Callable
 import wpimath
+from wpimath.geometry import Pose2d, Rotation2d
 import math
 
 nt = ntcore.NetworkTableInstance.getDefault()
+
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self): 
@@ -33,6 +36,18 @@ class MyRobot(wpilib.TimedRobot):
             wpimath.geometry.Translation3d(5.23, 4.03, 1.12),
             wpimath.geometry.Rotation3d.fromDegrees(0.0, 0.0, 0.0),
         )
+        self.field = Field2d()
+        SmartDashboard.putData("Field", self.field)
+        self.field.getObject("Target").setPose(
+            Pose2d(4.655, 4.019, 0.0)
+        )
+        self.controller = wpilib.XboxController(0)
+        self.x = 0.0
+        self.y = 0.0
+        self.heading = 0.0
+        self.maxSpeed = 3.0      # m/s
+        self.maxTurn = 2.5       # rad/s
+        self.lastTime = wpilib.Timer.getFPGATimestamp()
 
     def teleopPeriodic(self):
         targetYaw = 0.0
@@ -50,7 +65,7 @@ class MyRobot(wpilib.TimedRobot):
                 self.networkTargetX.setFloat(target_pose.X())
                 targetYaw = math.atan(target_pose.Y()/target_pose.X()) / 2 /math.pi
                 
-                #print(self.target_pose.Y())
+        #print(self.target_pose.Y())
         #print("x =" + str(self.target_pose.X()))
         #print("\n")
 
@@ -63,3 +78,23 @@ class MyRobot(wpilib.TimedRobot):
         self.pitchservo_pos += targetPitch
         self.yawservo.set(self.yawservo_pos)
         self.pitchservo.set(self.pitchservo_pos)
+
+        #field stuff
+        #self.field.setRobotPose(self.odometry.getPose())
+
+        # #controller stuff
+        # now = wpilib.Timer.getFPGATimestamp()
+        # dt = now - self.lastTime
+        # self.lastTime = now
+
+        # forward = -self.controller.getLeftY() * self.maxSpeed
+        # turn = -self.controller.getRightX() * self.maxTurn
+
+        # self.heading += turn * dt
+
+        # self.x += forward * math.cos(self.heading) * dt
+        # self.y += forward * math.sin(self.heading) * dt
+
+        # self.field.setRobotPose(
+        #     Pose2d(self.x, self.y, Rotation2d(self.heading))
+        # )
