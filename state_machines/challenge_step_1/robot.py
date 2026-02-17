f06ba661ec727f0705a9ac869259331d90f53299
Kiwi <Cakeykai@gmail.com>
2026-02-16 19:48:28 -0700

    Cleaned up code :)

--- state_machines/challenge_step_1/robot.py
+++ state_machines/challenge_step_1/robot.py
@@ -25,11 +25,6 @@ class MyRobot(wpilib.TimedRobot):
         self.camPoseEst = photonlibpy.PhotonPoseEstimator(field,kRobotToCam,)
         print(nt.getTopics())
         self.camera = photonlibpy.PhotonCamera("Arducam_OV9281_USB_Camera")
-        self.yawservo = rev.SparkMax(23,rev.SparkLowLevel.MotorType.kBrushless)
-        self.pitchservo = wpilib.Servo(1)
-        self.yawservo_pos = 0.5
-        self.pitchservo_pos = 0.5
-        self.cameraAngle = 0.5
         self.target_pose = wpimath.geometry.Pose3d(
             wpimath.geometry.Translation3d(4.625594, 4.034663, 1.8288),
             wpimath.geometry.Rotation3d.fromDegrees(0.0, 0.0, 0.0),
@@ -43,15 +38,10 @@ class MyRobot(wpilib.TimedRobot):
         self.field.getObject("Target").setPose(
             Pose2d(4.655, 4.019, 0.0)
         )
-        # self.jordanMotor = rev.SparkMax(23, rev.SparkLowLevel.MotorType.kBrushless)
-        # self.jordanMotorEncoder = self.jordanMotor.getEncoder()
 
 
 
     def teleopPeriodic(self):
-        
-        targetYaw = 0.0
-        targetPitch = 0.0
         self.counter.setInteger(self.counter.getInteger(0) + 1)
         
         results = self.camera.getAllUnreadResults()
@@ -63,33 +53,3 @@ class MyRobot(wpilib.TimedRobot):
             if camEstPose is not None:
                 target_pose = self.target_pose - camEstPose.estimatedPose
                 self.networkTargetX.setFloat(target_pose.X())
-                targetYaw = math.atan(target_pose.Y()/target_pose.X()) / 2 /math.pi #find it
-                targetYaw = targetYaw + self.cameraAngle - self.yawservo.getAbsoluteEncoder().getPosition()
-                # self.field.setRobotPose(camEstPose) #eventually will be odometry pose. Camera for now is center of robot.
-                
-        self.yawservo_pos += targetYaw
-        self.pitchservo_pos += targetPitch
-        self.yawservo.set(self.yawservo_pos)
-        #self.yawservo.set()
-        self.pitchservo.set(self.pitchservo_pos)
-
-        #self.jordanMotor.set(0.3)
-
-        #field stuff -- where setrobotpose originally was
-
-        # #controller stuff
-        # now = wpilib.Timer.getFPGATimestamp()
-        # dt = now - self.lastTime
-        # self.lastTime = now
-
-        # forward = -self.controller.getLeftY() * self.maxSpeed
-        # turn = -self.controller.getRightX() * self.maxTurn
-
-        # self.heading += turn * dt
-
-        # self.x += forward * math.cos(self.heading) * dt
-        # self.y += forward * math.sin(self.heading) * dt
-
-        # self.field.setRobotPose(
-        #     Pose2d(self.x, self.y, Rotation2d(self.heading))
-        # )
