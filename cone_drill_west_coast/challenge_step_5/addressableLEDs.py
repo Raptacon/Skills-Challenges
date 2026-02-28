4fe7c26d15b864667ae63b1bd6fe90eb3014ad69
nazeermazeer <234304154+nazeermazeer@users.noreply.github.com>
2026-02-28 15:35:36 -0700

    "sonar" illumination pattern for code and length=10

--- cone_drill_west_coast/challenge_step_5/addressableLEDs.py
+++ cone_drill_west_coast/challenge_step_5/addressableLEDs.py
@@ -3,16 +3,29 @@ import wpilib
 class addressableLEDs:
     def __init__(self):
         #Ensure LEDs are connected to PWM, not DIO.
+        self.lengthLED = 10
+        self.activatedLED = 1
+        self.movementLED = 1
+        
         self.led = wpilib.AddressableLED(9)
-        self.led.setLength(30)
+        self.led.setLength(self.lengthLED)
         self.led_buffer = []
 
     def lightLEDs(self):
         self.led_buffer = []
-        for i in range(30):
+        for i in range(self.lengthLED):
             led_data = wpilib.AddressableLED.LEDData()
-            led_data.setLED(wpilib.Color.kAzure)
+            if i == self.activatedLED:
+                led_data.setLED(wpilib.Color.kAzure)
+            else:
+                led_data.setLED(wpilib.Color.kBlack)
             # d_data.setRGB(255, 0, 0)
             self.led_buffer.append(led_data)
         self.led.setData(self.led_buffer)
         self.led.start()
+
+        if self.activatedLED == self.lengthLED or self.activatedLED >= self.lengthLED:
+            self.movementLED = -1
+        if self.activatedLED == 0 or self.activatedLED <= 0:
+            self.movementLED = 1
+        self.activatedLED += self.movementLED
